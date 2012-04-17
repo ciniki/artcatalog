918913bb7803cf3d702bc3cabba007243a294bfe
Andrew Rivett <veggiefrog@gmail.com>
2012-04-17 15:23:26 -0400

    Fixed bug with showing multiple categories when having lower and upper case for same name

--- public/searchField.php
+++ public/searchField.php
@@ -62,7 +62,7 @@ function ciniki_artcatalog_searchField($ciniki) {
 			. "AND " . $args['field'] . " <> '' "
 			. ") "
 		. "";
-	$strsql .= "ORDER BY name "
+	$strsql .= "ORDER BY " . $args['field'] . " "
 		. "";
 	if( isset($args['limit']) && $args['limit'] != '' && $args['limit'] > 0 ) {
 		$strsql .= "LIMIT " . ciniki_core_dbQuote($ciniki, $args['limit']) . " ";
