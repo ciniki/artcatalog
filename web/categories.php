f6c057f3470e62ea1b2cf92dbbebbdf4ca21556b
Andrew Rivett <veggiefrog@gmail.com>
2012-05-05 09:24:35 -0400

    Added category images for website

--- web/categories.php
+++ web/categories.php
@@ -26,9 +26,41 @@ function ciniki_artcatalog_web_categories($ciniki, $settings, $business_id) {
 		. "";
 	
     require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashQueryTree.php');
-	return ciniki_core_dbHashQueryTree($ciniki, $strsql, 'artcatalog', array(
+	$rc = ciniki_core_dbHashQueryTree($ciniki, $strsql, 'artcatalog', array(
 		array('container'=>'categories', 'fname'=>'name', 'name'=>'category',
 			'fields'=>array('name')),
 		));
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+	if( !isset($rc['categories']) ) {
+		return array('stat'=>'ok');
+	}
+	$categories = $rc['categories'];
+
+	//
+	// Load highlight images
+	//
+	foreach($categories as $cnum => $cat) {
+		//
+		// Look for the highlight image, or the most recently added image
+		//
+		$strsql = "SELECT ciniki_artcatalog.image_id, ciniki_images.image "
+			. "FROM ciniki_artcatalog, ciniki_images "
+			. "WHERE ciniki_artcatalog.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+			. "AND category = '" . ciniki_core_dbQuote($ciniki, $cat['category']['name']) . "' "
+			. "AND ciniki_artcatalog.image_id = ciniki_images.id "
+			. "ORDER BY ciniki_artcatalog.date_added DESC "
+			. "LIMIT 1";
+		$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'artcatalog', 'image');
+		if( $rc['stat'] != 'ok' ) {
+			return $rc;
+		}
+		if( isset($rc['image']) ) {
+			$categories[$cnum]['category']['image_id'] = $rc['image']['image_id'];
+		}
+	}
+
+	return array('stat'=>'ok', 'categories'=>$categories);	
 }
 ?>
