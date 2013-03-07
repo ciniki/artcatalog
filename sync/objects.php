aeb9a98642bdd1a4cbfe2125bcccdca753956d86
Andrew Rivett <veggiefrog@gmail.com>
2013-03-06 19:40:17 -0500

    Added sync to artcatalog

---
+++ sync/objects.php
@@ -0,0 +1,62 @@
+<?php
+//
+// Description
+// -----------
+//
+// Arguments
+// ---------
+//
+// Returns
+// -------
+//
+function ciniki_artcatalog_sync_objects($ciniki, &$sync, $business_id, $args) {
+	
+	//
+	// NOTES: When pushing a change, grab the history for the current session
+	// When increment/partial/full, sync history on it's own
+	//
+
+	//
+	// Working on version 2 of sync, completely object based
+	//
+	$objects = array();
+	$objects['item'] = array(
+		'name'=>'Art Catalog Item',
+		'table'=>'ciniki_artcatalog',
+		'fields'=>array(
+			'name'=>array(),
+			'permalink'=>array(),
+			'type'=>array(),
+			'flags'=>array(),
+			'webflags'=>array(),
+			'image_id'=>array('ref'=>'ciniki.images.image'),
+			'catalog_number'=>array(),
+			'category'=>array(),
+			'year'=>array(),
+			'media'=>array(),
+			'size'=>array(),
+			'framed_size'=>array(),
+			'price'=>array(),
+			'location'=>array(),
+			'awards'=>array(),
+			'notes'=>array(),
+			'description'=>array(),
+			'inspiration'=>array(),
+			'user_id'=>array('ref'=>'ciniki.users.user'),
+			),
+		'history_table'=>'ciniki_artcatalog_history',
+		);
+	$objects['tag'] = array(
+		'name'=>'Art Catalog Tag',
+		'table'=>'ciniki_artcatalog_tags',
+		'fields'=>array(
+			'artcatalog_id'=>array('ref'=>'ciniki.artcatalog.item'),
+			'tag_type'=>array(),
+			'tag_name'=>array(),
+			),
+		'history_table'=>'ciniki_artcatalog_history',
+		);
+	
+	return array('stat'=>'ok', 'objects'=>$objects);
+}
+?>
