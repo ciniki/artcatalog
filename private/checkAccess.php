0cd35cc4162c4acc64b034122db96e1778cb042c
Andrew Rivett <veggiefrog@gmail.com>
2014-02-23 17:27:37 -0500

    Added ciniki pointer to checkAccess

--- private/checkAccess.php
+++ private/checkAccess.php
@@ -15,7 +15,7 @@
 // =======
 // <rsp stat="ok" />
 //
-function ciniki_artcatalog_checkAccess($ciniki, $business_id, $method) {
+function ciniki_artcatalog_checkAccess(&$ciniki, $business_id, $method) {
 	//
 	// Check if the business is active and the module is enabled
 	//
