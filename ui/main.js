d3f2907c87fc43e24fd9248008ee86bac7fc7389
Andrew Rivett <veggiefrog@gmail.com>
2015-04-11 19:37:36 -0400

    Updated for pdf

--- ui/main.js
+++ ui/main.js
@@ -1897,7 +1897,7 @@ function ciniki_artcatalog_main() {
 		if( fields != '' ) {
 			args['fields'] = fields.substring(1);
 		}
-		M.api.openPDF(this.downloadpdf.method, args);
+		M.showPDF(this.downloadpdf.method, args);
 //		window.open(M.api.getUploadURL(this.downloadpdf.method, args));
 	};
 
