1dc0232fa2405e653b5bfe7049595b3e4354e8b0
Andrew Rivett <veggiefrog@gmail.com>
2017-04-24 08:53:15 -0400

    Fixed bugs in web indexing

--- hooks/webIndexObject.php
+++ hooks/webIndexObject.php
@@ -34,7 +34,7 @@ function ciniki_artcatalog_hooks_webIndexObject($ciniki, $business_id, $args) {
 
     if( $args['object'] == 'ciniki.artcatalog.item' ) {
         $strsql = "SELECT id, name, permalink, type, status, flags, webflags, category, "
-            . "image_id, media, size, framed_size, price, description, inspiration, awards "
+            . "image_id, year, media, size, framed_size, price, description, inspiration, awards "
             . "FROM ciniki_artcatalog "
             . "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
             . "AND id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
@@ -50,37 +50,42 @@ function ciniki_artcatalog_hooks_webIndexObject($ciniki, $business_id, $args) {
         //
         // Check if item is visible on website
         //
-        if( ($rc['item']['webflags']&0x01) == 0 ) {
+        $item = $rc['item'];
+        if( ($item['webflags']&0x01) == 0 ) {
             return array('stat'=>'ok');
         }
         $object = array(
-            'label'=>$rc['item']['category'],
-            'title'=>$rc['item']['name'],
+            'label'=>$item['category'],
+            'title'=>$item['name'],
             'subtitle'=>'',
             'meta'=>'',
-            'primary_image_id'=>$rc['item']['image_id'],
+            'primary_image_id'=>$item['image_id'],
             'synopsis'=>'',
             'object'=>'ciniki.artcatalog.item',
-            'object_id'=>$rc['item']['id'],
-            'primary_words'=>$rc['item']['name'] . ' ' . $rc['item']['category'] . ' ' . $rc['item']['size'],
+            'object_id'=>$item['id'],
+            'primary_words'=>$item['name'] . ' ' . $item['category'] . ' ' . $item['size'],
             'secondary_words'=>'',
             'tertiary_words'=>'',
             'weight'=>10000,
-            'url'=>'/gallery/category/' . urlencode($rc['item']['category']) . '/' . $rc['item']['permalink']
+            'url'=>'/gallery/category/' . urlencode($item['category']) . '/' . $item['permalink']
             );
 
-        if( $rc['item']['media'] != '' && ($rc['item']['webflags']&0x1000) > 0 ) {
-            $object['meta'] .= $rc['item']['media'];
-            $object['secondary_words'] .= ($object['secondary_words']!=''?' ':'') . $rc['item']['media'];
+        if( $item['year'] != '' && ($item['webflags']&0x8000) > 0 ) {
+            $object['meta'] .= ($object['meta'] != '' ? ', ' : '') . $item['year'];
+            $object['secondary_words'] .= ($object['secondary_words'] != '' ? ' ' : '') . $item['year'];
         }
-        if( ($rc['item']['webflags']&0x2000) > 0 ) {
+        if( $item['media'] != '' && ($item['webflags']&0x1000) > 0 ) {
+            $object['meta'] .= ($object['meta'] != '' ? ', ' : '') . $item['media'];
+            $object['secondary_words'] .= ($object['secondary_words'] != '' ? ' ' : '') . $item['media'];
+        }
+        if( ($item['webflags']&0x2000) > 0 ) {
             //
             // Get the list of materials
             //
             $strsql = "SELECT DISTINCT tag_name "
                 . "FROM ciniki_artcatalog_tags "
                 . "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
-                . "AND artcatalog_id = '" . ciniki_core_dbQuote($ciniki, $rc['item']['id']) . "' "
+                . "AND artcatalog_id = '" . ciniki_core_dbQuote($ciniki, $item['id']) . "' "
                 . "AND tag_type = 100 "
                 . "ORDER BY tag_name "
                 . "";
@@ -94,54 +99,54 @@ function ciniki_artcatalog_hooks_webIndexObject($ciniki, $business_id, $args) {
                 $object['secondary_words'] .= ($object['secondary_words']!=''?' ':'') . implode(' ', $rc['materials']);
             }
         }
-        if( $rc['item']['size'] != '' ) {
-            $object['meta'] .= ($object['meta']!=''?', ':'') . $rc['item']['size'];
+        if( $item['size'] != '' ) {
+            $object['meta'] .= ($object['meta'] != '' ? ', ' : '') . $item['size'];
         }
-        if( ($rc['item']['flags']&0x10) > 0 ) {
-            if( $rc['item']['size'] != '' ) {
-                if( $rc['item']['framed_size'] != '' ) {
-                    $object['meta'] .= ' (Framed: ' . $rc['item']['framed_size'] . ')';
+        if( ($item['flags']&0x10) > 0 ) {
+            if( $item['size'] != '' ) {
+                if( $item['framed_size'] != '' ) {
+                    $object['meta'] .= ' (Framed: ' . $item['framed_size'] . ')';
                 } else {
                     $object['meta'] .= ' Framed';
                 }
             } else {
-                if( $rc['item']['framed_size'] != '' ) {
-                    $object['meta'] .= ($object['meta']!=''?', ':'') . 'Framed: ' . $rc['item']['framed_size'] . '';
+                if( $item['framed_size'] != '' ) {
+                    $object['meta'] .= ($object['meta'] != '' ? ', ' : '') . 'Framed: ' . $item['framed_size'] . '';
                 } else {
-                    $object['meta'] .= ($object['meta']!=''?', ':'') . 'Framed';
+                    $object['meta'] .= ($object['meta'] != '' ? ', ' : '') . 'Framed';
                 }
             }
-        } elseif( $rc['item']['size'] != '' && $rc['item']['type'] == 1 && ($rc['item']['flags']&0x10) == 0 ) {
+        } elseif( $item['size'] != '' && $item['type'] == 1 && ($item['flags']&0x10) == 0 ) {
             $object['meta'] .= ' (unframed)';
         }
-        if( ($rc['item']['webflags']&0x0800) > 0 ) {
-            if( $rc['item']['status'] == 20 ) {
-                if( $rc['item']['price'] != '' && $rc['item']['price'] != '0' && $rc['item']['price'] != '0.00' ) {
-                    if( is_numeric($rc['item']['price']) ) {
-                        $rc['item']['price'] = numfmt_format_currency($ciniki['business']['settings']['intl-default-currency-fmt'], $rc['item']['price'], $ciniki['business']['settings']['intl-default-currency']);
-                        $object['meta'] .= ($object['meta']!=''?', ':'') . $rc['item']['price'] . ' ' . $ciniki['business']['settings']['intl-default-currency'];
-                        $object['primary_words'] .= ' ' . sprintf("%d", $rc['item']['price']);
+        if( ($item['webflags']&0x0800) > 0 ) {
+            if( $item['status'] == 20 ) {
+                if( $item['price'] != '' && $item['price'] != '0' && $item['price'] != '0.00' ) {
+                    if( is_numeric($item['price']) ) {
+                        $item['price'] = numfmt_format_currency($ciniki['business']['settings']['intl-default-currency-fmt'], $item['price'], $ciniki['business']['settings']['intl-default-currency']);
+                        $object['meta'] .= ($object['meta']!=''?', ':'') . $item['price'] . ' ' . $ciniki['business']['settings']['intl-default-currency'];
+                        $object['primary_words'] .= ' ' . sprintf("%d", $item['price']);
                     } else {
-                        $object['meta'] .= ($object['meta']!=''?', ':'') . $rc['item']['price'];
-                        $object['primary_words'] .= ' ' . $rc['item']['price'];
+                        $object['meta'] .= ($object['meta']!=''?', ':'') . $item['price'];
+                        $object['primary_words'] .= ' ' . $item['price'];
                     }
                 }
             } else {
-                $object['meta'] .= ($object['meta']!=''?', ':'') . $maps['item']['status'][$rc['item']['status']];
+                $object['meta'] .= ($object['meta']!=''?', ':'') . $maps['item']['status'][$item['status']];
             }
         }
 
-        if( isset($rc['item']['description']) && $rc['item']['description'] != '' && ($rc['item']['webflags']&0x0100) > 0 ) {
-            $object['synopsis'] = $rc['item']['description'];
-            $object['tertiary_words'] .= ($object['tertiary_words']!=''?', ':'') . ' ' . $rc['item']['description'];
+        if( isset($item['description']) && $item['description'] != '' && ($item['webflags']&0x0100) > 0 ) {
+            $object['synopsis'] = $item['description'];
+            $object['tertiary_words'] .= ($object['tertiary_words']!=''?', ':'') . ' ' . $item['description'];
         }
 
-        if( isset($rc['item']['inspiration']) && $rc['item']['inspiration'] != '' && ($rc['item']['webflags']&0x0200) > 0 ) {
-            $object['tertiary_words'] .= ($object['tertiary_words']!=''?', ':'') . ' ' . $rc['item']['inspiration'];
+        if( isset($item['inspiration']) && $item['inspiration'] != '' && ($item['webflags']&0x0200) > 0 ) {
+            $object['tertiary_words'] .= ($object['tertiary_words']!=''?', ':'') . ' ' . $item['inspiration'];
         }
 
-        if( isset($rc['item']['awards']) && $rc['item']['awards'] != '' && ($rc['item']['webflags']&0x0400) > 0 ) {
-            $object['tertiary_words'] .= ($object['tertiary_words']!=''?', ':'') . ' ' . $rc['item']['awards'];
+        if( isset($item['awards']) && $item['awards'] != '' && ($item['webflags']&0x0400) > 0 ) {
+            $object['tertiary_words'] .= ($object['tertiary_words']!=''?', ':'') . ' ' . $item['awards'];
         }
 
         return array('stat'=>'ok', 'object'=>$object);
